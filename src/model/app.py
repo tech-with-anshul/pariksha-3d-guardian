0c4ef3b8220b73a31696bccc5b53978fcc2a7a01
tech-with-anshul <kanshulmussoorie@gmail.com>
2025-11-30 23:47:58 +0530

    Refactor pose prediction and attention monitoring logic; remove unused code and enhance head direction analysis

--- src/model/app.py
+++ src/model/app.py
@@ -16,29 +16,6 @@ app = Flask(__name__)
 multiple_people_detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/d0/1")
 
 
-
-# @app.route('/predict_people',methods=['GET','POST'])
-# def predict_pose():
-#     data = request.get_json()
-#     # get image tensor 
-#     output = multiple_people_detector(image, threshold = 0.5)
-#     people = 0
-#     for i in range(int(output['num_detections'][0])):
-#         if classes[i] == 1 and scores[i] > threshold:
-#             people += 1
-#             ymin, xmin, ymax, xmax = boxes[i]
-#             (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
-#                                           ymin * im_height, ymax * im_height)
-#             draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)],
-#                       width=4, fill='red')
-
-#     return jsonify({ 'people' : int(people) , 'image' : image})
-
-
-
-
-
-
 def readb64(uri):
    encoded_data = uri.split(',')[1]
    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
@@ -47,69 +24,192 @@ def readb64(uri):
    return img
 
 
-@app.route('/predict_pose', methods = ['GET', 'POST']) 
-def predict_pose() : 
-    data = request.get_json(force = True) 
+def get_head_direction(rotation_vector):
+    """
+    Analyze rotation vector to determine head direction.
+    rotation_vector[0] = pitch (up/down)
+    rotation_vector[1] = yaw (left/right)
+    rotation_vector[2] = roll (tilt)
+    """
+    pitch = rotation_vector[0][0]  # X-axis rotation (up/down)
+    yaw = rotation_vector[1][0]    # Y-axis rotation (left/right)
+    roll = rotation_vector[2][0]   # Z-axis rotation (tilt)
+    
+    # Thresholds for detection (in radians, adjust as needed)
+    PITCH_THRESHOLD = 0.3  # ~17 degrees
+    YAW_THRESHOLD = 0.4    # ~23 degrees
+    
+    direction = {
+        'looking_up': False,
+        'looking_down': False,
+        'looking_left': False,
+        'looking_right': False,
+        'looking_straight': True,
+        'pitch': float(pitch),
+        'yaw': float(yaw),
+        'roll': float(roll)
+    }
+    
+    # Check vertical direction (pitch)
+    if pitch < -PITCH_THRESHOLD:
+        direction['looking_up'] = True
+        direction['looking_straight'] = False
+    elif pitch > PITCH_THRESHOLD:
+        direction['looking_down'] = True
+        direction['looking_straight'] = False
+    
+    # Check horizontal direction (yaw)
+    if yaw < -YAW_THRESHOLD:
+        direction['looking_right'] = True
+        direction['looking_straight'] = False
+    elif yaw > YAW_THRESHOLD:
+        direction['looking_left'] = True
+        direction['looking_straight'] = False
+    
+    return direction
+
+
+@app.route('/predict_pose', methods=['GET', 'POST']) 
+def predict_pose(): 
+    data = request.get_json(force=True) 
     image = r'{}'.format(data['img'])
     print(type(image), image)
-    image= readb64(image)
+    image = readb64(image)
     plt.imshow(image)
-    # plt.show()
-    # plt.imsave(image, 'sample.jpg');
+    
     height, width = image.shape[0], image.shape[1]
     pose_estimator = PoseEstimator(img_size=(height, width))
     mark_detector = MarkDetector()
 
     facebox = mark_detector.extract_cnn_facebox(image)
-        # Any face found?
     frame = image
+    
     if facebox is not None:
-
-        # Step 2: Detect landmarks. Crop and feed the face area into the
-        # mark detector.
         x1, y1, x2, y2 = facebox
         face_img = frame[y1: y2, x1: x2]
 
-        # Run the detection.
         marks = mark_detector.detect_marks(face_img)
 
-        # Convert the locations from local face area to the global image.
         marks *= (x2 - x1)
         marks[:, 0] += x1
         marks[:, 1] += y1
 
-        # Try pose estimation with 68 points.
         pose = pose_estimator.solve_pose_by_68_points(marks)
+        
+        rotation_vector = pose[0]
+        translation_vector = pose[1]
+        
+        # Get head direction analysis
+        head_direction = get_head_direction(rotation_vector)
 
-        # All done. The best way to show the result would be drawing the
-        # pose on the frame in realtime.
+        img, pose_result = pose_estimator.draw_annotation_box(
+            frame, rotation_vector, translation_vector, color=(0, 255, 0)
+        )
+        
+        return jsonify({
+            'status': 'face_found',
+            'head_direction': head_direction,
+            'pose': {
+                'rotation': rotation_vector.tolist(),
+                'translation': translation_vector.tolist()
+            },
+            'warnings': get_warnings(head_direction)
+        })
+    else:
+        return jsonify({
+            'status': 'face_not_found',
+            'head_direction': None,
+            'warnings': ['No face detected in frame']
+        })
 
-        # Do you want to see the pose annotation?
-        img, pose = pose_estimator.draw_annotation_box(frame, pose[0], pose[1], color=(0, 255, 0))
 
-        # Do you want to see the head axes?
-        # pose_estimator.draw_axes(frame, pose[0], pose[1])
+def get_warnings(direction):
+    """Generate warning messages based on head direction."""
+    warnings = []
+    
+    if direction['looking_up']:
+        warnings.append('Student is looking UP - possible cheating detected')
+    if direction['looking_down']:
+        warnings.append('Student is looking DOWN - possible cheating detected')
+    if direction['looking_left']:
+        warnings.append('Student is looking LEFT - possible cheating detected')
+    if direction['looking_right']:
+        warnings.append('Student is looking RIGHT - possible cheating detected')
+    
+    if not warnings:
+        warnings.append('Student is looking at screen - OK')
+    
+    return warnings
 
-        # Do you want to see the marks?
-        # mark_detector.draw_marks(frame, marks, color=(0, 255, 0))
 
-        # Do you want to see the facebox?
-        # mark_detector.draw_box(frame, [facebox])
-        img = list(img)
-        return jsonify({'img' : 'face found', 'pose' : pose})
-    else :
-        return jsonify({'message' : 'face not found', 'img' : 'img'})
+@app.route('/check_attention', methods=['GET', 'POST'])
+def check_attention():
+    """
+    Simplified endpoint specifically for attention monitoring.
+    Returns whether student is paying attention to screen.
+    """
+    data = request.get_json(force=True)
+    image = readb64(data['img'])
+    
+    height, width = image.shape[0], image.shape[1]
+    pose_estimator = PoseEstimator(img_size=(height, width))
+    mark_detector = MarkDetector()
+    
+    facebox = mark_detector.extract_cnn_facebox(image)
+    
+    if facebox is None:
+        return jsonify({
+            'attention': False,
+            'reason': 'no_face',
+            'message': 'No face detected',
+            'severity': 'high'
+        })
+    
+    x1, y1, x2, y2 = facebox
+    face_img = image[y1: y2, x1: x2]
+    marks = mark_detector.detect_marks(face_img)
+    
+    marks *= (x2 - x1)
+    marks[:, 0] += x1
+    marks[:, 1] += y1
+    
+    pose = pose_estimator.solve_pose_by_68_points(marks)
+    direction = get_head_direction(pose[0])
+    
+    is_attentive = direction['looking_straight']
+    
+    reason = 'attentive'
+    severity = 'none'
+    
+    if direction['looking_up']:
+        reason = 'looking_up'
+        severity = 'medium'
+    elif direction['looking_down']:
+        reason = 'looking_down'
+        severity = 'medium'
+    elif direction['looking_left']:
+        reason = 'looking_left'
+        severity = 'high'
+    elif direction['looking_right']:
+        reason = 'looking_right'
+        severity = 'high'
+    
+    return jsonify({
+        'attention': is_attentive,
+        'reason': reason,
+        'direction': direction,
+        'severity': severity,
+        'message': 'Student is attentive' if is_attentive else f'Student is {reason.replace("_", " ")}'
+    })
 
 
-
-
-@app.route('/predict_people',methods=['GET','POST'])
-def predict() : 
-    data = request.get_json(force = True)
-    image= readb64(data['img'])
+@app.route('/predict_people', methods=['GET', 'POST'])
+def predict(): 
+    data = request.get_json(force=True)
+    image = readb64(data['img'])
     im_width, im_height = image.shape[0], image.shape[1]
     image = image.reshape((1, image.shape[0], image.shape[1], 3))
-    # return jsonify({'data' : data})
+    
     data = multiple_people_detector(image)
 
     boxes = data['detection_boxes'].numpy()[0]
@@ -121,28 +221,22 @@ def predict() :
     for i in range(int(data['num_detections'][0])):
         if classes[i] == 1 and scores[i] > threshold:
             people += 1
-            ymin, xmin, ymax, xmax = boxes[i]
-            (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
-                                          ymin * im_height, ymax * im_height)
-            # draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)],
-            #           width=4, fill='red')
 
-    return jsonify({ 'people' : int(people) , 'image' : 'image'})
+    return jsonify({'people': int(people), 'image': 'image'})
 
 
 @app.route('/save_img', methods=['GET', 'POST']) 
-def save() : 
-    data = request.get_json(force = True)
+def save(): 
+    data = request.get_json(force=True)
     image = r'{}'.format(data['img'])
     user = data['user']
-    image= readb64(image)
+    image = readb64(image)
     base_dir = os.getcwd()
-    path = r"{}\images\{}.jpg".format(base_dir, user[0:-10])
+    path = r"{}/images/{}.jpg".format(base_dir, user[0:-10])
     print(path)
-    plt.imsave(image, path)
-    return jsonify({'path' : path})
+    plt.imsave(path, image)
+    return jsonify({'path': path})
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
